0f3738c3b8f2e42d7ffff34b4c9f5744754322d4
anshulAtStack <anshul.chanchlani@wipro.com>
2016-04-28 11:41:46 +0530

    included header

--- common-ui/div.jsx
+++ common-ui/div.jsx
@@ -1,9 +1,8 @@
  import React from 'react';
  import TopicsContainer from './TopicsContainer';
- import TextField from 'material-ui/TextField';
- import IconButton from 'material-ui/IconButton';
+ import SearchBar from './searchBar';
 
- var topicsData =[ {
+ var topics =[ {
    title: "Sports Category",
    subtitle: "This is the sports category",
    text: "Welcome to the Sports Category. Here you will find various sub categories to play quizzes in."
@@ -25,54 +24,12 @@
  }
 ];
 
- var topicsData1 =[ {
-   title: "Sports Category",
-   subtitle: "This is the sports category",
-   text: "Welcome to the Sports Category. Here you will find various sub categories to play quizzes in."
- },
- {
-   title: "TV Series Category",
-   subtitle: "This is the tv series category",
-   text: "Welcome to the tv series Category. Here you will find various sub categories to play quizzes in."
- }
-
-];
-var style={
-  float:'right'
-}
-
 var TopicPage = React.createClass({
-  getInitialState: function(){
-
-    return {allTopics: topicsData};
-  },
-  filterTopics: function(event){
-
-      var outerThis = this;
-       var rows =[];
-       topicsData.forEach(function(topic){
-         if(topic.title.toLowerCase().indexOf(event.target.value.toLowerCase())!==-1)
-            rows.push(topic);
-
-       });
-       outerThis.setState({allTopics:rows});
-
-
-
-  },
-
   render: function(){
     return(
       <div>
-        <TopicsContainer topics={this.state.allTopics} />
-          <div style={style}>
-             <TextField
-               hintText="Hint Text" onChange={this.filterTopics}
-             />
-           <IconButton>
-           <i className="material-icons">search</i>
-           </IconButton>
-           </div>
+        <TopicsContainer topics={topics} />
+        <SearchBar />
       </div>
     );
   }

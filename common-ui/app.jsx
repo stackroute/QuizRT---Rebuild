673917ab25c48093a448a2e02f8dbee5b7d1e9d3
Rohith <rohith.gonela@wipro.com>
2016-04-28 11:05:29 +0530

    Dashboard header

--- common-ui/app.jsx
+++ common-ui/app.jsx
@@ -9,6 +9,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+<<<<<<< HEAD
 import LoginView from './views/LoginView';
 import SignupView from './views/SignupView';
 import TournamentView from './views/Tournament';
@@ -22,17 +23,29 @@ export class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <Router history={hashHistory}>
-        <Route path="/" component={LoginView} />
-        <Route path="/signup" component={SignupView} />
-        <Route path="/topics" component={TopicsView} />
-        <Route path="/tournament" component={TournamentView} />
+        <Router history={hashHistory}>
+          <Route path="/" component={LoginView} />
+          <Route path="/signup" component={SignupView} />
+          <Route path="/topics" component={TopicsView} />
+          <Route path="/tournament" component={TournamentView} />
 
 
-       </Router>
+        </Router>
       </MuiThemeProvider>
     );
   }
 };
- 
+
+=======
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import ProfileHero from './header/leftnav'
+var muiTheme = getMuiTheme();
+
+const App = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <ProfileHero />
+  </MuiThemeProvider>
+);
+
+>>>>>>> Dashboard header
 ReactDOM.render(<App />, document.getElementById('content'));

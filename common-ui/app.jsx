12477d636ee8c6bed78eeb900029f7b18b4e56e6
de326729 <deepak950dk@gmail.com>
2016-04-28 18:38:42 +0530

    Login First Commit

--- common-ui/app.jsx
+++ common-ui/app.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Router, Route, hashHistory } from 'react-router';
-
+ import Login from './login';
+import NewPass from './setNewPass';
+import signup from './signup1';
+import VerifyOTP from './verifyOTP';
+import ForgotPass from './forgotPass';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-
-<<<<<<< HEAD
 import LoginView from './views/LoginView';
 import SignupView from './views/SignupView';
 import TournamentView from './views/Tournament';
 import TopicsView from './views/TopicsPage';
-
-
 
 const DashboardView = () => <div>This is the dashboard.</div>
 
@@ -39,13 +37,22 @@ export class App extends React.Component {
 =======
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 import ProfileHero from './header/leftnav'
+=======
+>>>>>>> Login First Commit
 var muiTheme = getMuiTheme();
 
 const App = () => (
   <MuiThemeProvider muiTheme={muiTheme}>
+<<<<<<< HEAD
     <ProfileHero />
   </MuiThemeProvider>
 );
 
 >>>>>>> Dashboard header
+=======
+     <signup />
+  </MuiThemeProvider>
+);
+
+>>>>>>> Login First Commit
 ReactDOM.render(<App />, document.getElementById('content'));
---
+++ common-ui/login.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Checkbox from 'material-ui/Checkbox';
+import Dialog from 'material-ui/Dialog';
+
+var style = {
+
+  margin: 12,
+  marginLeft:0,
+  width: 256
+};
+var first = {
+ margin:'auto',
+ width:300
+
+};
+
+var second = {
+
+	marginRight :0,
+
+};
+var third = {
+	textAlign :'center',
+
+}
+
+var Login = React.createClass({
+  render: function() {
+    return (
+			<div style = {first}>
+        <h1 style={third}>quizRT</h1>
+				<p style ={third}>Login here to play the game</p>
+    	  <TextField hintText = "Email Field"	floatingLabelText = "Email"	type = "text" /><br/>
+        <TextField hintText = "Password Field" floatingLabelText = "Password" type = "password" /><br/><br/>
+        <Checkbox label = "Remember Me" /><br/><br/>
+
+        <RaisedButton label = "Login" primary = {true} style = {style} /><br/>
+
+        <p style={second} ><a href="#" >Forgot Password</a></p>
+				<p style={third}>OR</p>
+        <RaisedButton label = "Login With Facebook" secondary = {true} style = {style} /><br/>
+        <RaisedButton label = "Login With Google" secondary = {true} style = {style} /><br/>
+    	</div>
+    );
+  }
+});
+
+module.exports = Login;

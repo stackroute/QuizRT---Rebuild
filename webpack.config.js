6d94d45bf1289a8b490370748c276c6f82a000ff
Sagar Patke <sagarpatke@gmail.com>
2016-04-30 13:43:08 +0530

    Fixed material-ui-icons. Font Icons from Material UI Docs are now avaiable for use.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
           presets: ['es2015', 'react']
 
         },
-        exclude: __dirname + '/node_modules'
+
+        exclude: /(node_modules|bower_components)/
 
       }
     ]
@@ -25,8 +26,13 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
+
     /*new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),*/
     new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}})
+
+    new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
+    new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}}),
+ 
   ],
   resolve: {
     extensions: ['','.js','.jsx']

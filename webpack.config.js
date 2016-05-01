47bd7677abfb179adc7964c50df42a65fd086ca4
Sagar Patke <sagarpatke@gmail.com>
2016-05-01 07:12:39 +0530

    Removed Minification for verbose error messages.

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
-    new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}}),
+    /*new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),*/
+    new webpack.optimize.UglifyJsPlugin({compressor: {warnings: false}})
   ],
   resolve: {
     extensions: ['','.js','.jsx']

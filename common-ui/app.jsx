4bc41762ccc868a4fb82696115859be0b4a68d45
shikharatri <shikharatri1@gmail.com>
2016-05-05 14:59:08 +0530

    Error Correction

--- common-ui/app.jsx
+++ common-ui/app.jsx
@@ -4,7 +4,6 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import LoginView from './views/LoginView';
-import SignupView from './views/SignupView';
 import TournamentView from './views/Tournament';
 import TopicsView from './views/TopicsPage';
 import {Router, Route, hashHistory} from 'react-router';
@@ -12,6 +11,7 @@ import LoginForm from './views/LoginView';
 import ForgotPswd from './views/LoginView/forgotPswd';
 import VerifyOTP from './views/LoginView/verifyOTP';
 import SetNewPswd from './views/LoginView/setNewPswd';
+import Badges from './views/Badges/Badges';
 injectTapEventPlugin();
 
 const DashboardView = () => <div>This is the dashboard.</div>
@@ -20,18 +20,17 @@ export class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <Router history={hashHistory}>
-        <Route path="/dashboard" component={DashboardView} />
-        <Route path="/" component={LoginView} />
-        <Route path="/login" component={LoginView} />
-        <Route path="/signup" component={SignupView} />
-        <Route path="/tournament" component={TournamentView} />
-        <Route path="/topics" component={TopicsView} />
-        <Route path = '/' component = {LoginForm} />
-    	<Route path = '/forgotPswd' component = {ForgotPswd} />
-    	<Route path = '/setNewPswd' component = {SetNewPswd} />
-    	<Route path = '/verifyOTP' component = {VerifyOTP} />
-      </Router>
+        <Router history={hashHistory}>
+          <Route path="/dashboard" component={DashboardView} />
+          <Route path="/" component={LoginView} />
+          <Route path="/login" component={LoginView} />
+          <Route path="/tournament" component={TournamentView} />
+          <Route path="/topics" component={TopicsView} />
+          <Route path = '/' component = {LoginForm} />
+          <Route path = '/forgotPswd' component = {ForgotPswd} />
+          <Route path = '/setNewPswd' component = {SetNewPswd} />
+          <Route path = '/verifyOTP' component = {VerifyOTP} />
+        </Router>
       </MuiThemeProvider>
     );
   }
--- common-ui/views/LoginView/login.jsx
+++ common-ui/views/LoginView/login.jsx
@@ -15,30 +15,29 @@ export default class LoginForm extends React.Component{
   render() {
     return (
       <div className = "container-fluid">
-      <div className="row">
-          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-lg-offset-4 col-md-offset-4 col-sm-offset-3">
-          
-        <h1>Login</h1>
-        <h1>QuizRT</h1>
-				<p>Login here to play the game</p>
-        <form>
-    	  <TextField hintText = "Email Field"	floatingLabelText = "Email"	type = "text" style = {styles}/><br/>
-        <TextField hintText = "Password Field" floatingLabelText = "Password" type = "password" style = {styles}/><br/><br/>
-        </form>
-        <Checkbox label = "Remember Me" /><br/><br/>
+        <div className="row">
+            <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-lg-offset-4 col-md-offset-4 col-sm-offset-3">
+            
+              <h1>Login</h1>
+              <h1>QuizRT</h1>
+  				    <p>Login here to play the game</p>
+              <form>
+      	       <TextField hintText = "Email Field"	floatingLabelText = "Email"	type = "text" style = {styles}/><br/>
+                <TextField hintText = "Password Field" floatingLabelText = "Password" type = "password" style = {styles}/><br/><br/>
+              </form>
+              <Checkbox label = "Remember Me" /><br/><br/>
 
-        <RaisedButton label = "Login" primary = {true} style = {styles}/><br/>
-        <Link to ='/forgotPswd'>
-        <p>Forgot Password</p>
-        </Link>
-				<p>OR</p>
-        <RaisedButton label = "Login With Facebook" secondary = {true} style = {styles}/><br/><br/>
-        <RaisedButton label = "Login With Google" secondary = {true} style = {styles}/><br/><br/>
-        <RaisedButton label = "Create Account" primary = {true} style = {styles}/><br/>
-        </Paper>
-    	</div>
-   </div>
-  </div>
+              <RaisedButton label = "Login" primary = {true} style = {styles}/><br/>
+              <Link to ='/forgotPswd'>
+                <p>Forgot Password</p>
+              </Link>
+  				    <p>OR</p>
+              <RaisedButton label = "Login With Facebook" secondary = {true} style = {styles}/><br/><br/>
+              <RaisedButton label = "Login With Google" secondary = {true} style = {styles}/><br/><br/>
+              <RaisedButton label = "Create Account" primary = {true} style = {styles}/><br/>
+      	    </div>
+        </div>
+      </div>
 
     );
   }

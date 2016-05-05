14b9ed516ffe72feb49ed1fb8c65a20c23236a97
komalimallidi <komali.mallidi@wipro.com>
2016-05-05 15:07:24 +0530

    Folder Structure Changed

---
+++ common-ui/views/SubTopics/SubTopicCard.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+//import {orange500} from 'material-ui/styles/colors';
+
+
+var style1= {
+
+  background:'#c6ecc6'
+}
+
+var cardDivStyle={
+   width:"80%",
+   float:'left',
+
+   padding:10,
+   margin:10
+
+}
+
+var title1={
+    "font-size":"100%"
+}
+var title2={
+  "font-size":"95%"
+}
+var title3={
+
+    width:"80%",
+    margin:'auto'
+}
+var title4={
+     width:"30%",
+
+     margin:'auto'
+   }
+var SubTopicCard = React.createClass({
+  render: function(){
+    return(
+
+
+<Card style ={cardDivStyle}>
+
+    <CardHeader
+      title={this.props.topic.title}
+      titleStyle ={title1}
+      subtitle={this.props.topic.subtitle}
+      subtitleStyle ={title2}
+      avatar={this.props.topic.avatarImg}
+    />
+
+    <CardMedia style={title3}>
+      <img src={this.props.topic.img} />
+    </CardMedia>
+
+
+    <CardActions >
+
+        <div className="col-xs col-md col-lg col-sm">
+        <RaisedButton label="+Fav" primary={true} style={title4}   />
+        <RaisedButton label="Play" style={title4} secondary={true}  />
+        </div>
+  </CardActions>
+
+    </Card>
+
+  );
+}
+});
+
+module.exports= SubTopicCard;

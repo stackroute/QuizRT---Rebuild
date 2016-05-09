a5f20bc4e09417ba53973aa17757f6d6dec88a9d
shikharatri <shikharatri1@gmail.com>
2016-05-09 11:30:09 +0530

    Final Changes made

--- common-ui/views/SubTopics/SubTopicContainer.jsx
+++ common-ui/views/SubTopics/SubTopicContainer.jsx
@@ -123,7 +123,7 @@ var SubTopicContainer = React.createClass({
 
   render: function(){
       var allTheTopics = [];
-      this.props.topic.forEach(function(topic){
+      this.props.topics.forEach(function(topic){
         allTheTopics.push(
             <div className="col-lg-3 col-xs-12 col-md-3 col-sm-3">
             <SubTopicCard topic={topic} key={topic.title}/>

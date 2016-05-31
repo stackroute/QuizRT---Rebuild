8a3e64872facaaa7e8a2ac0b635606735100ce06
Anshul jain <anshuldpma@gmail.com>
2016-05-31 14:21:12 +0530

    get all topic

---
+++ microservices/getTopic/getAllTopic.js
@@ -0,0 +1,11 @@
+module.exports = function(){
+  this.add('role:topic,action:allTopic',function(msg,respond){
+    this.make$('topics_collection').native$(function (err, db) {
+      if(err) return respond(err);
+      var collection = db.collection('topics_collection');
+      collection.distinct("topicName",{topicName:{$exists:true}},function(err,documents){
+        respond(null,documents)
+      })
+  })
+})
+}

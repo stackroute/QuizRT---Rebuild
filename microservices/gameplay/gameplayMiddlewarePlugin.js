63b6a65a1a22f36ea5e0f2009b8efd83df311025
anshulAtStack <anshul.chanchlani@wipro.com>
2016-06-02 04:37:00 +0000

    Updated plugin for gameplay provisioner api

--- microservices/gameplay/gameplayMiddlewarePlugin.js
+++ microservices/gameplay/gameplayMiddlewarePlugin.js
@@ -1,12 +1,23 @@
 module.exports = function gamePlayMiddleware(options){
   var self = this;
-
+  var id = Math.random()*2343242;
   self.username = options.username;
   self.tournamentId = options.tournamentId;
   self.socket = options.socket;
 
+  setTimeout(function(){
+    self.act({role:'tournament',id:self.tournamentId,username:self.username,gameId:id},function(err,response){
+
+    })
+  },1000)
+
+
   this.add('role:tournament,cmd:queue',function(msg,respond){
+
     respond(null,{answer:'queued'});
     self.socket.emit('tournamentQueue',{msg:'queued'});
   });
+
+
+
 }

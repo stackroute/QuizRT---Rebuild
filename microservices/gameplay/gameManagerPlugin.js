92acc7c9feecde14b4588b0a238aee8388727542
anshulAtStack <anshul.chanchlani@wipro.com>
2016-06-04 12:12:43 +0000

    Game Manager and Client Middleware setup.

--- microservices/gameplay/gameManagerPlugin.js
+++ microservices/gameplay/gameManagerPlugin.js
@@ -1,6 +1,74 @@
 module.exports = function(options){
-  var id = Math.random() * 233421124;
-  this.add('role:gameManager,action:create',function(err,response){
-        respond(null,{gameId:id})
-  })
+  var self = this;
+  var broadcastPin = 'action:broadcast,gameId:'+options.id;
+  self.gameId = options.id
+  self.broadcast = require('seneca')()
+                   .use('redis-transport')
+                   .client({type:'redis',pin:broadcastPin})
+                   .ready(function() {
+                     console.log('==========================READY TO SEND OVER PIN: ' + broadcastPin);
+                   });
+var pingCount = options.users.length;
+  // Create  response channels on which game manager will listen.
+  self.users = options.users.map(function(user) {
+    var userMicroservice = require('seneca')();
+
+        userMicroservice.use('redis-transport');
+        console.log('\n \n Setting up microservice for: '+ user);
+        userMicroservice.add('role:'+user+',gameId:'+self.gameId,function(msg,respond){
+
+          respond(null,{answer:'listening to game'});
+        });
+
+
+        userMicroservice.add('role:' + user + ',gameId:' + self.gameId+',action:ping', function(msg,respond) {
+          respond(null,{msg:'pong'});
+          console.log('\n Ping received at server sending pong \n');
+
+
+          if(pingCount>0){
+            pingCount--;
+
+          }
+          if(pingCount==0){
+
+            console.log('\n Sending broadcast pingcount is 0\n');
+
+             var pin = 'action:broadcast,gameId:'+self.gameId;
+             //self.broadcast
+            var msg = 'action:broadcast,gameId:'+self.gameId+',answer:Starting'
+            console.log('\n==========================SENDING OVER MSG: ',msg);
+            self.broadcast.act(msg,function(err,response){
+                             console.log('\n'+response.user+ ' is listening to this broadcast');
+                           });
+          }
+
+
+        });
+
+        userMicroservice.listen({type:'redis',pin:'role:'+user+',gameId:'+self.gameId});
+
+    return userMicroservice;
+  });
+
+  //Broadcast on pin 'gameId:<gameId>,role:*'
+
+
+
+
+
+
+
+
+  // Broadcast service.
+  // self.broadcast = require('seneca')();
+  // console.log('\n Broadcasting ');
+  // self.broadcast.add('role:gameManager,action:broadcast,gameId:'+self.gameId,function(msg,respond){
+  //
+  //   respond(null,{answer:'broadcasting'});
+  // })
+  // self.broadcast.use('redis-transport');
+  // self.broadcast.listen({type:'redis',pin:'role:gameManager,action:broadcast,gameId:'+self.gameId});
+
+
 }
--- microservices/gameplay/topicCreatorPlugin.js
+++ microservices/gameplay/topicCreatorPlugin.js
@@ -1,7 +1,12 @@
 module.exports = function(options){
-  this.add('role:playerChannel,action:create',function(msg,respond){
-    console.log(msg.username);
-    console.log(msg.tournamentId);
-    respond(null,{answer:'player publishing to gameplay'});
+
+  var id = Math.random()*342323;
+  var self = this;
+  self.id = options.id;
+  self.username = options.username;
+
+  this.add({role:'tournament',id:self.id,username:self.username,gameId:id},function(msg,respond){
+
+    respond(null,{answer:'player publishing to gameplay with gameID: '+id});
   })
 }

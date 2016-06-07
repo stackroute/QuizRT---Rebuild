bb566a9683afb57c9a3a9ab3beb508042f5b2380
Anshul jain <anshuldpma@gmail.com>
2016-06-07 13:29:10 +0530

    shell file

---
+++ microservices.sh
@@ -0,0 +1,7 @@
+node microservices/mesh/baseMicroservice.js &node microservices/allTopics/allTopicsService.js &
+node microservices/allTournaments/allTournamentsService.js &
+node microservices/LoginAuthentication/registerUser/registerUserMicroservice.js &
+node microservices/LoginAuthentication/verifyToken/verifyTokenMicroservice.js &
+node microservices/question/questionMicroservice.js &
+node microservices/tournamentSection/tournamentService.js &
+node microservices/mostPopTopicSection/popularTopicService.js

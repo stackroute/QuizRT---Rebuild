a5846c0cf8e1fabacb06cee7f6bb919ba58c0355
Sagar Patke <sagarpatke@gmail.com>
2016-06-11 15:20:20 +0530

    Docker Compose for Deployment

--- microservices/randomQuestion/randomQuestionService.js
+++ microservices/randomQuestion/randomQuestionService.js
@@ -1,10 +1,16 @@
 var seneca = require('seneca');
 var randomQuestionService = seneca();
 randomQuestionService.use('entity');
+
+console.log('MONGO_DB_NAME: ' + process.env.MONGO_DB_NAME);
+console.log('MONGO_HOST: ' + process.env.MONGO_HOST);
+console.log('MONGO_PORT: ' + process.env.MONGO_PORT);
+
 randomQuestionService.use('mongo-store',{
-  name:"quizRT4",
-  host:"127.0.0.1",
-  port:"27017"
-})
+  name:process.env.MONGO_DB_NAME,
+  host:process.env.MONGO_HOST,
+  port:process.env.MONGO_PORT
+});
+
 randomQuestionService.use('./randomQuestionPlugin');
-randomQuestionService.use('mesh',{auto:true,pin:'role:question,action:random'})
+randomQuestionService.use('mesh',{base: true, auto:true,pin:'role:question,action:random'})
